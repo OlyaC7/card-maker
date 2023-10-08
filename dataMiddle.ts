4bd7beffb19015609da34b8bcb92c48ecb33fac0
OlyaC7 <oagrig26@gmail.com>
2023-10-08 18:28:23 +0300

    добавление данных и выполнение проверки линтерами

--- dataMiddle.ts
+++ dataMiddle.ts
@@ -1,83 +1,91 @@
-import { FontStyleType, PictureType, TextType, CanvasType, EditorType, FigureType, Template } from "./types"
+import {
+  FontStyleType,
+  PictureType,
+  TextType,
+  CanvasType,
+  EditorType,
+  FigureType,
+  Template,
+} from './types'
 
 const picture1: PictureType = {
-    id: '1',
-    pozition: {
-        x: 100,
-        y: 100,
-    },
-    size: {
-        width: 700,
-        height: 100,
-    },
-    type: 'picture',
-    data: 'https://prixoxo.ru/uploads/posts/2021-09/krasivye-kartinki-priroda-na-rabochij-stol-1920h1080-9.jpg',
-    pictureType: 'link'
+  id: '1',
+  pozition: {
+    x: 100,
+    y: 100,
+  },
+  size: {
+    width: 700,
+    height: 100,
+  },
+  type: 'picture',
+  data: 'https://prixoxo.ru/uploads/posts/2021-09/krasivye-kartinki-priroda-na-rabochij-stol-1920h1080-9.jpg',
+  pictureType: 'link',
 }
 
 const fontStyle1: FontStyleType = {
-    bold: true,
-    italic: false
+  bold: true,
+  italic: false,
 }
 
 const text1: TextType = {
-    type: 'text',
-    id: '2',
-    text: 'Nemo enim ipsam voluptatem, unde omnis iste natus error sit voluptatem accusantium doloremque',
-    color: '#564897',
-    fontFamily: 'Roboto',
-    fontSize: '22px',
-    fontStyle: fontStyle1,
-    pozition: {
-        x: 50,
-        y: 220,
-    },
-    size: {
-        width: 50,
-        height: 22
-    }
+  type: 'text',
+  id: '2',
+  text: 'Nemo enim ipsam voluptatem, unde omnis iste natus error sit voluptatem accusantium doloremque',
+  color: '#564897',
+  fontFamily: 'Roboto',
+  fontSize: '22px',
+  fontStyle: fontStyle1,
+  pozition: {
+    x: 50,
+    y: 220,
+  },
+  size: {
+    width: 50,
+    height: 22,
+  },
 }
 
 const figure1: FigureType = {
-    type: 'figure',
-    id: '5',
-    fillColor: '#4BA3B6',
-    dataFigure: '<svg xmlns="http://www.w3.org/2000/svg" width="182" height="172" viewBox="0 0 182 172" fill="none"><path d="M91 0L181.933 171.75H0.0673294L91 0Z" </svg>',
-    borderColor: '#4BA3B6',
-    pozition: {
-        x: 600,
-        y: 120,
-    },
-    size: {
-        width: 182,
-        height: 172
-    }
+  type: 'figure',
+  id: '5',
+  fillColor: '#4BA3B6',
+  dataFigure:
+    '<svg xmlns="http://www.w3.org/2000/svg" width="182" height="172" viewBox="0 0 182 172" fill="none"><path d="M91 0L181.933 171.75H0.0673294L91 0Z" </svg>',
+  borderColor: '#4BA3B6',
+  pozition: {
+    x: 600,
+    y: 120,
+  },
+  size: {
+    width: 182,
+    height: 172,
+  },
 }
 
 const canvas: CanvasType = {
-    background: {
-        type: 'color',
-        data: '#500500',
-    },
-    size: {
-        width: 300,
-        height: 500,
-    },
-    objects: [picture1, text1, figure1]
+  background: {
+    type: 'color',
+    data: '#500500',
+  },
+  size: {
+    width: 300,
+    height: 500,
+  },
+  objects: [picture1, text1, figure1],
 }
 
 const template1: Template = {
-    id: '',
-    objects: []
+  id: '',
+  objects: [],
 }
 
 const newCanvas: EditorType = {
-    canvas: canvas,
-    templates: template1,
-    selectBlock: ['1','2']
+  canvas: canvas,
+  templates: template1,
+  selectBlock: ['1', '2'],
 }
-
 
 //текст короткий длинный
 //картинки все виды
-//холст где все 
+//холст где все
--- types.ts
+++ types.ts
@@ -1,12 +1,12 @@
 type EditorType = {
-    canvas: CanvasType
-    templates: Template
-    selectBlock: Array<string>
+  canvas: CanvasType
+  templates: Template
+  selectBlock: Array<string>
 }
 
 type Template = {
-    id: string
-    objects: Array<CanvasType>
+  id: string
+  objects: Array<CanvasType>
 }
 
 type Filter = 'red' | 'green' | 'blue' | 'whiteBlack'
@@ -14,68 +14,68 @@ type Filter = 'red' | 'green' | 'blue' | 'whiteBlack'
 //если пустой канвас, то значения null
 
 type CanvasType = {
-    objects: Array<PictureType | TextType | FigureType>
-    background: Picture
-    size: SizeType
+  objects: Array<PictureType | TextType | FigureType>
+  background: BackgroundType
+  size: SizeType
 }
 
-type Picture = {
-    type: 'link' | 'base64' | 'color'
-    data: string
+type BackgroundType = {
+  type: 'link' | 'base64' | 'color'
+  data: string
 }
 
 type ObjectType = {
-    id: string
-    pozition: PositionType
-    size: SizeType
+  id: string
+  pozition: PositionType
+  size: SizeType
 }
 
 type PositionType = {
-    x: number
-    y: number
+  x: number
+  y: number
 }
 
 type SizeType = {
-    width: number
-    height: number
+  width: number
+  height: number
 }
 
 type PictureType = ObjectType & {
-    type: 'picture'
-    pictureType: 'link' | 'base64'
-    data: string
+  type: 'picture'
+  pictureType: 'link' | 'base64'
+  data: string
 }
 
 type TextType = ObjectType & {
-    type: 'text'
-    text: string
-    color: string
-    fontFamily: string
-    fontSize: string
-    fontStyle: FontStyleType
+  type: 'text'
+  text: string
+  color: string
+  fontFamily: string
+  fontSize: string
+  fontStyle: FontStyleType
 }
 
 type FontStyleType = {
-    bold: boolean
-    italic: boolean
+  bold: boolean
+  italic: boolean
 }
 
 type FigureType = ObjectType & {
-    type: 'figure'
-    fillColor: string
-    dataFigure: string
-    borderColor: string
+  type: 'figure'
+  fillColor: string
+  dataFigure: string
+  borderColor: string
 }
 
 export {
-    EditorType,
-    Template,
-    CanvasType,
-    ObjectType,
-    PictureType,
-    TextType,
-    FigureType,
-    Picture,
-    SizeType,
-    FontStyleType
+  EditorType,
+  Template,
+  CanvasType,
+  ObjectType,
+  PictureType,
+  TextType,
+  FigureType,
+  BackgroundType,
+  SizeType,
+  FontStyleType,
 }
